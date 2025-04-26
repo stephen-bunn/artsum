8a78831a665143723f24fc81fdb12e812037895b
Stephen Bunn <stephen@bunn.io>
2025-04-26 12:08:37 -0400

    chore: adding missing docstring to calculate_blake2b512
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- src/checksum/blake.rs
+++ src/checksum/blake.rs
@@ -11,6 +11,7 @@ pub async fn calculate_blake2b256(options: &ChecksumOptions) -> Result<Vec<u8>, 
     Ok(hasher.finalize().to_vec())
 }
 
+/// Calculates the Blake2b512 checksum of a file.
 pub async fn calculate_blake2b512(options: &ChecksumOptions) -> Result<Vec<u8>, Error> {
     let mut hasher = blake2::Blake2b512::new();
     process_file(options.to_processing_options(|chunk| hasher.update(chunk))).await?;
